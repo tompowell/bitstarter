be9da3e013c894e28362b91867f06294aaea827d
Tom Powell <tompowell@gmail.com>
2013-07-13 03:14:11 +0000

    first shot

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var indexfile = fs.readFile('index.html');
+
+var buffer = new Buffer(indexfile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
